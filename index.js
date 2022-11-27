4356ed2469bdfc69498ab86227c2353428696bab
mahadi215 <mahadihasanbd215@gmail.com>
2022-11-27 16:28:18 +0600

    added jwt fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const jwt = require('jsonsonwevtoken');
+const jwt = require('jsonsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
